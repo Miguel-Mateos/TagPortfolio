a9dc3bec7efc01250a17b508c8f65d9a59eb8e30
Eneko96 <imr.aem2526@gmail.com>
2022-06-23 00:04:10 +0200

    blabla

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
@@ -7,12 +6,32 @@ function App() {
     <div className="App">
       <header className='header'>
         <div className='header-content'>
-          <h1>Welcome To My Gadeem <span>Portfolio</span></h1>
+          <h1>Welcome To My <span>Portfolio</span></h1>
         </div>
         <picture>
           <img className='header-image' src='/first_image.jpeg'/>
         </picture>
       </header>
+      <section>
+        <div className='subtitle-container'>
+          <h2>Put Random Learning Quote Here</h2>
+        </div>
+      </section>
+      <section style={{ marginTop: '2rem', display: 'flex', gap: '2rem', justifyContent: 'center' }}>
+        <span id='react'>
+          <img className='icon-image' src='/react.png' />
+        </span>
+        <span id='react'>
+          <img className='icon-image' src='/react.png' />
+        </span>
+        <span id='react'>
+          <img className='icon-image' src='/react.png' />
+        </span>
+        <span id='react'>
+          <img className='icon-image' src='/react.png' />
+        </span>
+
+      </section>
     </div>
   )
 }
