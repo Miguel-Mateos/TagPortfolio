f9372fa7401d9a564a7e494e58dd121cb75f42cc
Eneko96 <imr.aem2526@gmail.com>
2022-06-26 15:00:56 +0200

    added container to markdown

--- src/Markdown.tsx
+++ src/Markdown.tsx
@@ -10,9 +10,8 @@ export const Markdown = ({ repo }: { repo: string }) => {
     const response = await fetch("https://raw.githubusercontent.com/eneko96/"+repo.toLowerCase()+"/main/README.md");
     const status = await response.status;
     if (status === 404) {
-      setMarkdown('patata')
+      setMarkdown('# No Readme Found')
       setLoading(false)
-      // create placeholder markkdown?
       return 
     }
     const text = await response.text();
@@ -26,7 +25,6 @@ export const Markdown = ({ repo }: { repo: string }) => {
   }, [repo])
 
   if (!markdown) return null
-  // if (loading) return <div style={{ maxHeight: '500px', width: '300px'}} className='card'>Loading ...</div>
   return (
     <div className="markdown">
       <ReactMarkdown className="markdown" children={markdown} />
