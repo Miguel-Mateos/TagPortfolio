69d2ab4b45d7c408f7f5503eb3e4570d390a3287
Eneko96 <imr.aem2526@gmail.com>
2022-08-09 16:10:28 +0200

    Add Text Carousel component for displaying about me content

---
+++ src/Components/TextCarousel/TextCarousel.tsx
@@ -0,0 +1,37 @@
+import { FC, useEffect, useState } from 'react'
+import './styles.css'
+
+interface ICarouselProps {
+  title: string
+  arr: string[]
+}
+
+export const TextCarousel: FC<ICarouselProps> = ({ title, arr }) => {
+  const [active, setActive] = useState<number>(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActive((active + 1) % arr.length)
+    }, 5000)
+
+    return () => clearInterval(interval)
+  }, [])
+
+  return (
+    <div className="text-carousel-container">
+      <h2 className="text-carousel-subtitle">{title}</h2>
+      <div className="text-carousel-text-container">
+        {arr.map((employee, index) => (
+          <h3
+            key={`${index}-employee`}
+            style={{
+              display: active !== index ? 'none' : 'block'
+            }}
+          >
+            {employee}
+          </h3>
+        ))}
+      </div>
+    </div>
+  )
+}
--- src/Modules/About/About.tsx
+++ src/Modules/About/About.tsx
@@ -1,9 +1,27 @@
+import { useEffect, useRef, useState } from 'react'
+import { TextCarousel } from '../../Components/TextCarousel/TextCarousel'
+import './about.css'
+
+const asEmployeeArray = [
+  'Own central, complex components of the Front application',
+  'Drive and deliver on our product roadmap , turning rough specs into features',
+  'Find elegant solutions to usability problems',
+  'Provide leadership and mentorship to other engineers on the team',
+  'Ramp on new web technologies that will make Front better'
+]
+
+const asPersonArray = [
+  'I am a self-taught developer with a passion for web development.'
+]
+
 export const About = () => {
   return (
     <section className="default-section">
       <div className="description">
         <h1 className="title-box">About Me</h1>
-        <p>Soy el puto amo y os callais la puta boquita pringaos</p>
+        <p>I am a self-taught developer with a passion for web development.</p>
+        <TextCarousel title="As a Person" arr={asPersonArray} />
+        <TextCarousel title="As an Employee" arr={asEmployeeArray} />
       </div>
     </section>
   )
