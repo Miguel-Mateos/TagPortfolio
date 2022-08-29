7e12ada86f24862899f1ed3d71800ec8c8e6901e
Eneko96 <imr.aem2526@gmail.com>
2022-08-29 12:29:08 +0200

    maintenance of page

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,29 @@ import './index.css'
 import { Testing } from './Testing'
 const env = import.meta.env.VITE_ENV
 
+const Maintainment = () => {
+  return (
+    <main
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        height: '100vh'
+      }}
+    >
+      <h1>Page Under Maintenance</h1>
+      <picture>
+        <source srcSet="/tree.png" media="(max-width: 600px)" />
+        <img src="/tree.png" style={{ height: '10vh', width: 'auto' }} />
+      </picture>
+    </main>
+  )
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AppProvider>{env === 'isolated' ? <Testing /> : <App />}</AppProvider>
+    {/* <AppProvider>{env === 'isolated' ? <Testing /> : <App />}</AppProvider> */}
+    <Maintainment />
   </React.StrictMode>
 )
