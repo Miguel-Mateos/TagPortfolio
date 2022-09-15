4492955dc2d555a59a553458e06ea5dc2391709b
Eneko96 <imr.aem2526@gmail.com>
2022-09-15 17:35:11 +0100

    removed maintainment

--- src/main.tsx
+++ src/main.tsx
@@ -3,27 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { AppProvider } from './Context/ContextApi'
 import './index.css'
-const env = import.meta.env.VITE_ENV
-
-const Maintainment = () => {
-  return (
-    <main
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexDirection: 'column',
-        height: '100vh'
-      }}
-    >
-      <h1>Page Under Maintenance</h1>
-      <picture>
-        <source srcSet="/tree.png" media="(max-width: 600px)" />
-        <img src="/tree.png" style={{ height: '10vh', width: 'auto' }} />
-      </picture>
-    </main>
-  )
-}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
