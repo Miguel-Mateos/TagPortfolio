3b0ce8fc0b7a978a37b771fa4e5c95499e3a7daf
Eneko96 <imr.aem2526@gmail.com>
2022-09-15 17:31:40 +0100

    removed maintainment

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { AppProvider } from './Context/ContextApi'
 import './index.css'
-import { Testing } from './Testing'
 const env = import.meta.env.VITE_ENV
 
 const Maintainment = () => {
@@ -28,7 +27,8 @@ const Maintainment = () => {
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* <AppProvider>{env === 'isolated' ? <Testing /> : <App />}</AppProvider> */}
-    <Maintainment />
+    <AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>
 )
