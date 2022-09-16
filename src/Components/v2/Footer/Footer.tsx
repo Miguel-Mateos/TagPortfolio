2c9189dc23367ea2bc427dcc81a9839a42892a8b
Eneko96 <imr.aem2526@gmail.com>
2022-09-16 10:24:48 +0100

    refactor styles of footer

--- src/Components/v2/Footer/Footer.tsx
+++ src/Components/v2/Footer/Footer.tsx
@@ -1,37 +1,29 @@
 import { handleDownloadResumee } from '../../../utils/downloadCV'
+import './styles.css'
 
 export const Footer = () => {
   return (
-    <div
-      style={{
-        zIndex: 3,
-        position: 'relative',
-        padding: '80px 144px',
-        display: 'flex',
-        justifyContent: 'space-between',
-        backgroundColor: 'var(--neutral800)'
-      }}
-    >
-      <div style={{ display: 'flex', gap: '32px', color: 'var(--neutral0)' }}>
+    <footer className="footer-container">
+      <nav className="footer-nav">
         <a
           href="https://www.linkedin.com/in/i%C3%B1igo-moreno-ramos-175928167/"
           target="_blank"
-          style={{ color: 'var(--neutral0)', textTransform: 'capitalize' }}
+          className="footer-nav-anchor"
         >
           <small>Linkedin</small>
         </a>
-        <small onClick={handleDownloadResumee} style={{ cursor: 'pointer' }}>
-          Download CV
-        </small>
+        <a className="footer-nav-anchor" onClick={handleDownloadResumee}>
+          <small style={{ cursor: 'pointer' }}>Download CV</small>
+        </a>
         <a
           href="https://github.com/Eneko96"
           target="_blank"
-          style={{ color: 'var(--neutral0)', textTransform: 'capitalize' }}
+          className="footer-nav-anchor"
         >
           <small>Github</small>
         </a>
-      </div>
-      <small style={{ color: 'var(--neutral500)' }}>Iñigo Moreno 2022</small>
-    </div>
+      </nav>
+      <small className="footer-name">Iñigo Moreno 2022</small>
+    </footer>
   )
 }
