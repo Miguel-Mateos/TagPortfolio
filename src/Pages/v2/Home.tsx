2e695b14de05c758990a79035b985d2370f0f8ea
Eneko96 <imr.aem2526@gmail.com>
2022-09-22 13:17:00 +0100

    fixed call

--- src/Pages/v2/Home.tsx
+++ src/Pages/v2/Home.tsx
@@ -8,48 +8,50 @@ import { useAppContextV2 } from '@Context/ContextV2'
 
 export const Home = () => {
   const { baseData } = useAppContextV2()
-  return (
-    <div>
-      <HeadLine title="Hey welcome to my portfolio!" />
-      <div className="content home-container">
-        <div className="home-hero-container">
-          <div className="home-hero-brief">
-            <h1>
-              I am {baseData.name}, {baseData.position}
-            </h1>
-            <div className="small-title">
-              Experience In the Following industries
+  if (baseData)
+    return (
+      <div>
+        <HeadLine title="Hey welcome to my portfolio!" />
+        <div className="content home-container">
+          <div className="home-hero-container">
+            <div className="home-hero-brief">
+              <h1>
+                I am {baseData.name}, {baseData.position}
+              </h1>
+              <div className="small-title">
+                Experience In the Following industries
+              </div>
+              <div className="base">{baseData.experience}</div>
+              <br />
+              <div className="small-title">Core skills</div>
+              <div className="base">{baseData.skills}</div>
+              <br />
+              <div className="small-title">Cliffton Strengths</div>
+              <div className="base">{baseData.strengths}</div>
             </div>
-            <div className="base">{baseData.experience}</div>
-            <br />
-            <div className="small-title">Core skills</div>
-            <div className="base">{baseData.skills}</div>
-            <br />
-            <div className="small-title">Cliffton Strengths</div>
-            <div className="base">{baseData.strengths}</div>
+            <div className="home-hero-image">
+              <picture>
+                <source type="image/webp" srcSet="/first_image.webp" />
+                <source type="image/jpeg" srcSet="/first_image.jpg" />
+                <source type="image/webp" srcSet={baseData.image} />
+                <img
+                  src="/first_image.jpg"
+                  loading="eager"
+                  alt="head image"
+                  className="header-image"
+                />
+              </picture>
+            </div>
           </div>
-          <div className="home-hero-image">
-            <picture>
-              <source type="image/webp" srcSet="/first_image.webp" />
-              <source type="image/jpeg" srcSet="/first_image.jpg" />
-              <source type="image/webp" srcSet={baseData.image} />
-              <img
-                src="/first_image.jpg"
-                loading="eager"
-                alt="head image"
-                className="header-image"
-              />
-            </picture>
-          </div>
+          <div className="separator" />
+          <Section />
+          <Studies />
+          <Stack />
+          <CaseStudies />
         </div>
-        <div className="separator" />
-        <Section />
-        <Studies />
-        <Stack />
-        <CaseStudies />
       </div>
-    </div>
-  )
+    )
+  return null
 }
 
 // image could be retrieved from supabase
