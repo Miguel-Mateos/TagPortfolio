0e093b6f14c6c81ad5c02ca219fa7fddc881bea2
Eneko96 <imr.aem2526@gmail.com>
2022-10-11 23:37:01 +0100

    fix bookcard test

--- src/__test__/BookCard.test.tsx
+++ src/__test__/BookCard.test.tsx
@@ -1,13 +1,19 @@
-import { describe, it, expect } from 'vitest'
-import { render, screen } from '@testing-library/react'
+import { describe, it, expect, afterEach } from 'vitest'
+import { cleanup, render, screen } from '@testing-library/react'
 import { BookSelector } from '@Pages/v2/Book/BookCard'
 
 describe('BookSelector', () => {
+  afterEach(cleanup)
   it('should be defined', () => {
     expect(BookSelector).toBeDefined()
   })
   it('should render', () => {
     render(<BookSelector />)
-    expect(screen.getByText(/Choose next slot available/i)).toBeTruthy()
+    expect(screen.getByText('Choose Next Slot Available')).toBeTruthy()
+  })
+
+  it('should have 5 card', () => {
+    render(<BookSelector />)
+    expect(screen.getAllByText('17:30 PM').length).toBe(5)
   })
 })
