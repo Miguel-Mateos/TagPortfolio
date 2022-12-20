48281de289140325154671bbb4d9cc673d81937c
Eneko96 <imr.aem2526@gmail.com>
2022-12-20 20:31:32 +0100

    instructions

--- README.md
+++ README.md
@@ -1,3 +1,44 @@
-# Portfolio
+# Miguel Mateos Portfolio
 
-@Miguel-Mateos
+## Como usar
+
+### Instalar dependencias
+
+```bash
+npm install
+```
+
+### Iniciar servidor de desarrollo
+
+```bash
+npm run dev
+```
+### Subir a git
+
+```bash
+git add .
+git commit -m "Mensaje"
+git push
+```
+
+#### Para subir a git
+- Necesitas tener instalado git en tu ordenador
+- si tienes mac, entras en la terminal y escribes git, automaticamente te preguntara si quieres instalar xcode no se que pues lo instalas y ya esta
+- cuando hagas push te pedira usuario y contraseña,
+- el usuario es el tuyo
+- la contraseña tienes que ir a tu cuenta de github
+  - Icono arriba a la derecha
+  - Settings
+  - Developer settings (Sidebar izquierdo)
+  - Personal access tokens
+  - Creas token clasico con los permisos que quieras (en principio los que vienen por defecto creo que son suficientes, si no todo lo que sea write y read)
+  - pones ese token en donde el password de la terminal
+
+
+## Sitios para modificar la pagina
+
+- Archivo Case studies para los projects (tu local)
+- Archivo Footer para el footer (tu local)
+- Stack para los logos de las tecnologias (en db pones el nombre de la tecnologia y lo pones en true -- teck_stack)
+- Section para exp laboral (en la bd tienes las columnas para poner los datos -- la tabla es work)
+- Studies para certificaciones y referencias (bd igual que Section -- la tabla se llama cert_ref)
--- src/Components/v2/CaseStudies/CaseStudies.tsx
+++ src/Components/v2/CaseStudies/CaseStudies.tsx
@@ -1,14 +1,8 @@
-import Loader from '@TagDs/components/loader/loader'
-import { useEffect, useMemo, useState } from 'react'
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Card } from '../Card/Card'
 import { SeeMore } from '../SeeMore/SeeMore'
 import './styles.css'
-import { tmpSort } from './utils/tmpSort'
-
-const TEMP_NAMES = ['100_projects', 'BidsSocket', 'chat-app']
-const OWNER = 'Eneko96'
-const BRANCH = 'main'
 
 export const CustomChip: React.FC<any> = ({ children, className }) => (
   <small title={children} className={`custom-chip ${className ?? ''}`}>
@@ -29,95 +23,55 @@ export interface IRepos {
 }
 
 export const CaseStudies = () => {
-  const [repos, setRepos] = useState<IRepos[]>([])
   const [more, setMore] = useState(false)
 
   const toggleMore = () => setMore(!more)
 
-  useEffect(() => {
-    const getRepos = async () => {
-      const repos = await fetch(import.meta.env.VITE_GITHUB_URI)
-      const res = await repos.json()
-      tmpSort(res)
-      setRepos(res)
-    }
-    getRepos()
-  }, [])
-
-  const memoRepos = useMemo(
-    () => (more ? repos : repos.slice(0, 3)),
-    [repos, more]
-  )
-
   const navigate = useNavigate()
 
-  if (repos) {
-    return (
-      <div className="case-studies-container" id="casestudies">
-        <h2 className="case-studies-title">Projects</h2>
-        <div className="case-studies">
-          {memoRepos.map(
-            ({
-              id,
-              clone_url: _,
-              homepage,
-              name,
-              private_repo,
-              description,
-              created_at,
-              languages_url,
-              topics
-            }) => (
-              <Card key={id} className="case-studies-card">
-                <div className="card-img-container">
-                  <img
-                    className="card-img"
-                    // src={`https://raw.githubusercontent.com/${OWNER}/${name}/${BRANCH}/.github/assets/preview.png`}
-                    src={
-                      TEMP_NAMES.includes(name)
-                        ? `/${name}.png`
-                        : 'https://picsum.photos/500?random=2.webp'
-                    }
-                    alt="project-image"
-                    loading="lazy"
-                  />
-                </div>
-                <CustomChip clas>
-                  {private_repo ? 'private' : 'public' + ' Repository'}
-                </CustomChip>
-                <div className="card-title">{name}</div>
-                <div className="card-description-container">
-                  <div className="subtitle">{homepage}</div>
-                  <div className="medium card-description-inner">
-                    {description ??
-                      'There is no short description at the moment'}
-                  </div>
-                </div>
-                <button
-                  className="button card-button-details"
-                  onClick={() =>
-                    navigate('Study/' + name, {
-                      state: {
-                        title: name,
-                        owner: OWNER,
-                        repo: name,
-                        branch: BRANCH,
-                        created_at,
-                        languages_url,
-                        topics
-                      }
-                    })
-                  }
-                >
-                  See Details
-                </button>
-              </Card>
-            )
-          )}
-        </div>
-        <SeeMore more={more} setMore={toggleMore} className="study-see-more" />
+  return (
+    <div className="case-studies-container" id="casestudies">
+      <h2 className="case-studies-title">Projects</h2>
+      <div className="case-studies">
+        <Card className="case-studies-card">
+          <div className="card-img-container">
+            <img
+              className="card-img"
+              // src={`https://raw.githubusercontent.com/${OWNER}/${name}/${BRANCH}/.github/assets/preview.png`}
+              src="https://picsum.photos/500?random=2.webp"
+              alt="project-image"
+              loading="lazy"
+            />
+          </div>
+          <CustomChip>public Repository</CustomChip>
+          <div className="card-title">Title</div>
+          <div className="card-description-container">
+            <div className="subtitle">Subtitle</div>
+            <div className="medium card-description-inner">
+              There is no short description at the moment
+            </div>
+          </div>
+          <button
+            className="button card-button-details"
+            onClick={() =>
+              navigate('Study/' + 'Loquesea', {
+                state: {
+                  title: 'Title',
+                  owner: 'Miguel Mateos',
+                  repo: 'Nombre',
+                  branch: 'main',
+                  created_at: '2021-01-01',
+                  languages_url: '',
+                  topics: []
+                }
+              })
+            }
+          >
+            See Details
+          </button>
+        </Card>
       </div>
-    )
-  }
-  return <Loader automatic />
+      <SeeMore more={more} setMore={toggleMore} className="study-see-more" />
+    </div>
+  )
 }
